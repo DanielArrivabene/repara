efdfafbb155feb234b4fc65b7340c718ef3a4ac4
DanielArrivabene <139562422+DanielArrivabene@users.noreply.github.com>
2024-01-31 13:55:51 -0300

    update Media Query

--- repara/src/components/about/About.jsx
+++ repara/src/components/about/About.jsx
@@ -5,10 +5,11 @@ import AboutImg from '../../assets/img/about-img.jpg'
 function About() {
   return (
     <section id="about" className="container">
-        <article className="row">
+        <article className="row gap-1">
             <div className="col-md-6 desctipton">
                 <h2>Sobre Nós</h2>
-                <p>Somos uma empresa especializada no reparo de celulares e parelhos móveis. Estamos há mais de 10 anos no mercado com alta satisfação dos nossos clientes.</p>
+                <p>Somos uma empresa especializada no reparo de aparelhos móveis, periféricos e computadores. Estamos há mais de 8 anos no mercado contanto com uma equipe de técnicos especialistas em otimização e reparo, venha até nós ou entre em contato para fazer um orçamento.</p>
+                <a href="#contact_form">Fale conosco</a>
             </div>
             <div className="col-md-5 image">
                 <img src={AboutImg} alt="sobre nós" />
--- repara/src/components/carousel/Carousel.jsx
+++ repara/src/components/carousel/Carousel.jsx
@@ -1,14 +1,20 @@
-import '../../styles/components/Carousel.scss'
-import CarouselControls from './CarouselControls'
-import CarouselItems from './CarouselItems'
+import '../../styles/components/Carousel.scss';
 
 function Carrousel() {
   return (
-    <section id="banner" className="carousel slide" data-bs-ride="carousel">
-      <CarouselControls />
-      <CarouselItems />
-  </section>
-  )
+    <section id="banner">
+      <article className="banner__data_container">
+        <div className="banner__data">
+          <h2>Reparo de eletrônicos</h2>
+          <p>
+            A mais de 8 anos no mercado com alta satisfação dos clientes.
+            Somos especialistas na manutenção de eletrônicos, desde celulares a PCs.
+          </p>
+          <a href="#contact_form">Entrar em contato</a>
+        </div>
+      </article>
+    </section>
+  );
 }
 
-export default Carrousel
+export default Carrousel;
